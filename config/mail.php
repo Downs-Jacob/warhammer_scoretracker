e42cab64ccc587a4a416ecc7eaaf45758046654b
Jacob Downs <67808444+Downs-Jacob@users.noreply.github.com>
2021-04-20 21:29:54 -0400

    mail configuration

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('smtp.sendgrid.net', env('MAIL_HOST', 'smtp.mailgun.org')),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('apikey', env('MAIL_USERNAME')),
+            'password' => env('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', env('MAIL_PASSWORD')),
             'timeout' => null,
             'auth_mode' => null,
         ],
