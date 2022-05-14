ddc9d7460215fc049ff412c2045e15c26eb56b8b
downs-Jacob <tua04072@gmail.com>
2022-05-13 20:40:16 -0400

    about rework

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,23 +2,12 @@
 
 @section('content')
 
-<div class="mx-auto max-w-6xl p-6 bg-white"> 
-    <div class="px-6 text-3xl lg:text-5xl">
+<div class="mx-auto max-w-3xl p-6 bg-white"> 
+    <div class="px-6 text-3xl mb-12 text-center">
         About the Creator
     </div>
-    <div class="lg:flex mx-auto max-w-6xl p-6 bg-white">
-        
-        <div class="lg:w-2/3 markdown-body lg:mr-6 mb-6 lg:mb-0">
-            <p class="justify-center text-justify"> My name is Jacob and I am an avid lover of Warhammer 40k, both as a hobby and as a game! I decided to build this website as a way to practice coding in PHP
-                but also as a way of keeping track of the games and statistics of all the games of Warhammer 40k that I play. 
-            </p>
-            <p class="justify-center text-justify">
-                I hope you also enjoy using this website for tracking your
-                own games of Warhammer 40k. For now, this website will only keep track of your games within the 9th edition ruleset but I want to expand the website to include other
-                editions, Kill Team, Necromunda, Age of Sigmar, and other GW game systems. If you have any suggestions feel free to contact me!
-            </p>
-        </div>
-        <div class="lg:w-2/3 markdown-body text-center lg:mr-6 mb-6 lg:mb-0">
+    <div class="mx-auto w-2/3 mb-12 px-4 bg-white">
+        <div class="mb-10">
             <img src="/images/abby.png"
             alt=""
             class="text-center"
@@ -26,6 +15,18 @@
             height="450px"
             >
         </div>
+        
+        <div class="justify-center text-justify mb-12 ">
+            <p class="justify-center text-justifym mb-12"> My name is Jacob and I am an avid lover of Warhammer 40k, both as a hobby and as a game! I decided to build this website as a way to practice coding in PHP
+                but also as a way of keeping track of the games and statistics of all the games of Warhammer 40k that I play. 
+            </p>
+            <p class="justify-center text-justify mb-12">
+                I hope you also enjoy using this website for tracking your
+                own games of Warhammer 40k. For now, this website will only keep track of your games within the 9th edition ruleset but I want to expand the website to include other
+                editions, Kill Team, Necromunda, Age of Sigmar, and other GW game systems. If you have any suggestions feel free to contact me!
+            </p>
+        </div>
+
     </div>
 </div>
 
