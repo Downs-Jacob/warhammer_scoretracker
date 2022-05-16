ee1da4495376649ac2c43cf485f5bc9c58ffff0e
downs-Jacob <tua04072@gmail.com>
2022-05-16 11:37:46 -0400

    remember old entries on registeration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                                 <label class="block mb-2 text-xs font-bold uppercase text-grey-700" for="username">
                                     Username
                                 </label>
-                                <input class="w-full p-2 border border-gray-400" type="text" name="username" id="username" required>
+                                <input class="w-full p-2 border border-gray-400" type="text" name="username" id="username" value="{{ old('username') }}" required>
 
                                 @error('username')
                                     <p class="mt-2 text-xs text-red-500">{{ $message }}</p>
@@ -24,7 +24,7 @@
                                 <label class="block mb-2 text-xs font-bold uppercase text-grey-700" for="name">
                                     name
                                 </label>
-                                <input class="w-full p-2 border border-gray-400" type="text" name="name" id="name" required>
+                                <input class="w-full p-2 border border-gray-400" type="text" name="name" id="name" value="{{ old('name') }}" required>
 
                                 @error('name')
                                     <p class="mt-2 text-xs text-red-500">{{ $message }}</p>
@@ -35,7 +35,7 @@
                                 <label class="block mb-2 text-xs font-bold uppercase text-grey-700" for="email">
                                     Email
                                 </label>
-                                <input class="w-full p-2 border border-gray-400" type="text" name="email" id="email"
+                                <input class="w-full p-2 border border-gray-400" type="text" name="email" id="email" value="{{ old('email') }}"
                                     autocomplete="email" required>
 
                                 @error('email')
