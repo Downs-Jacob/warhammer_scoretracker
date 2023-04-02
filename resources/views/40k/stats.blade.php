995cb0822a09cf4c7473a5caf324f171d593ecb3
downs-Jacob <tua04072@gmail.com>
2023-04-02 19:06:36 -0400

    update change

--- resources/views/40k/stats.blade.php
+++ resources/views/40k/stats.blade.php
@@ -54,11 +54,12 @@
                 
     </div>
     <div class="flex-2 shadow-xl px-4 py-2 m-2 text-center text-gray-700 bg-[#D0AAD8] border border-indigo-200 rounded-lg">
-        <h1 class="font-extrabold">Yearly Breakdown</h1>
-                @foreach($date_stats as $date_stats)
-                    <div class="flex-1 px-4 py-2 m-2 text-center text-gray-700 bg-[#EFE2F2]">Games played in 2023 : {{$date_stats->date_count}}</div>
-                @endforeach
-    </div>
+    <h1 class="font-extrabold">Yearly Breakdown</h1>
+        @foreach($date_stats as $stats)
+            <div class="flex-1 px-4 py-2 m-2 text-center text-gray-700 bg-[#EFE2F2]">Games played in {{$stats->year}} : {{$stats->date_count}}</div>
+        @endforeach
+</div>
+
     
 
 </div>
