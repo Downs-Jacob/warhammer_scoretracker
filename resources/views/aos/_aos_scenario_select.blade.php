25f5ed939b3c76068073e4b7b9bd500d9c5dee30
downs-Jacob <tua04072@gmail.com>
2023-04-03 14:54:27 -0400

    fix to aos

--- resources/views/aos/_aos_scenario_select.blade.php
+++ resources/views/aos/_aos_scenario_select.blade.php
@@ -13,7 +13,7 @@
 
                     <div class='px-6 py-3 m-2 border-8 border-gray-300' x-data="{
                         show: false,
-                        selection_scenario: null,
+                        selection_scenario: '{{ old('scenario') ?? '' }}',
                         scenarios: [
                             {
                                 name:'2023 GENERALS HANDBOOK',
@@ -47,11 +47,11 @@
                         </label>
                         
                         <select class="mr-2 px-4 py-2 border rounded" x-model="selection_scenario" name='scenario'>
-                            <option value={{'option'}}>--</option>
+                            <option value='option'>--</option>
                             <template x-for="scenario in scenarios">
                                 <optgroup :label="scenario.name">
                                     <template x-for="item in scenario.options" :key="item">
-                                        <option x-text="item" :value="item"></option>
+                                        <option x-text="item" :value="item" :selected="item === selection_scenario"></option>
                                     </template>
                                 </optgroup>
                             </template>
