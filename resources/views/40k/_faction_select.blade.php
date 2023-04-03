011001b4ecb46dd5680a1cf64e59c53bb9c9c656
downs-Jacob <tua04072@gmail.com>
2023-04-03 15:01:58 -0400

    faction change

--- resources/views/40k/_faction_select.blade.php
+++ resources/views/40k/_faction_select.blade.php
@@ -2,7 +2,7 @@
               <div class='' x-data="{
 
                     show: false,
-                    selection_faction: '{{ old('scenario') ?? '' }}',
+                    selection_faction: '{{ old($player.'_faction') ?? '' }}',
                     factions: [
                       {
                         name: 'SPACE MARINES',
@@ -64,22 +64,21 @@
               }"
                   <br>
 
-                  <div class="w-auto">
-                        <label class="flex-1 block mb-2 text-xs font-bold tracking-wide text-indigo-400 uppercase" for="grid-first-name">
-                          {{$player}} Faction
-                        </label>
-                        <select class="px-4 py-2 border rounded" x-model="selection_faction" name='{{$player}}_faction'>
-                            <option value=''>--</option>
-                            <template x-for="faction in factions">
-                                <optgroup :label="faction.name">
-                                    <template x-for="item in faction.options" :key="item">
-                                        <option x-text="item" :value="item" :selected="item === selection_faction"></option>
-                                    </template>
-                                </optgroup>
-                            </template>
-                        </select>
-
-                    </div>
-                  </div>
-
+                 
+    <div class="w-auto">
+        <label class="flex-1 block mb-2 text-xs font-bold tracking-wide text-indigo-400 uppercase" for="grid-first-name">
+            {{$player}} Faction
+        </label>
+        <select class="px-4 py-2 border rounded" x-model="selection_faction" name='{{$player}}_faction'>
+            <option value=''>--</option>
+            <template x-for="faction in factions">
+                <optgroup :label="faction.name">
+                    <template x-for="item in faction.options" :key="item">
+                        <option x-text="item" :value="item" :selected="item === selection_faction"></option>
+                    </template>
+                </optgroup>
+            </template>
+        </select>
+    </div>
+</div>
 
