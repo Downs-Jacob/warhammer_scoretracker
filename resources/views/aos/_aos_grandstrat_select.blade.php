efab654fa27f93e329a851f0a5ada08b1c3f1e3d
downs-Jacob <tua04072@gmail.com>
2023-04-03 17:30:03 -0400

    grandstrat select rework

--- resources/views/aos/_aos_grandstrat_select.blade.php
+++ resources/views/aos/_aos_grandstrat_select.blade.php
@@ -1,44 +1,36 @@
-
-              <div class='' x-data="{
-
-show: false,
-selection_grandstrat: '{{ old($player.'_grandstrat') ?? '' }}',
-grandstrats: [
-                      {
-                        name: 'GENERALS HANDBOOK 2023',
-                        options: [
-                          'Tame the Land',
-                          'Defend Whats Ours',
-                          'Take Whats Theirs',
-                          'Stake a Claim',
-                          'Survivors Instinct',
-                          'The Day is Ours',
-                          'Army Specific',
-                  
-                          ]
-                      }
-                      
-                  ],
-              }"
-<br>
-
-<div class="w-full">
+<div class='' x-data="{
+  show: false,
+  selection_grandstrat: null,
+  grandstrats: [
+    {
+      name: 'GENERALS HANDBOOK 2023',
+      options: [
+        'Tame the Land',
+        'Defend Whats Ours',
+        'Take Whats Theirs',
+        'Stake a Claim',
+        'Survivors Instinct',
+        'The Day is Ours',
+        'Army Specific',
+      ]
+    }
+  ]
+}" x-init="
+  selection_grandstrat = '{{ old($player.'_grandstrat', '') }}';
+">
+  <div class="w-full">
     <label class="block mb-2 text-xs font-bold tracking-wide text-indigo-400 uppercase" for="grid-grandstrat">
       {{$player}} grandstrat
     </label>
     <select class="px-4 py-2 border rounded" x-model="selection_grandstrat" name='{{$player}}_grandstrat'>
-        <option value={{'option'}}>--</option>
-        <template x-for="grandstrat in grandstrats">
-            <optgroup :label="grandstrat.name">
-                <template x-for="item in grandstrat.options" :key="item" :selected="item === selection_grandstrat">
-                    <option x-text="item" :value="item"></option>
-                </template>
-            </optgroup>
-        </template>
+      <option value={{'option'}}>--</option>
+      <template x-for="grandstrat in grandstrats">
+        <optgroup :label="grandstrat.name">
+          <template x-for="item in grandstrat.options" :key="item">
+            <option x-text="item" :value="item"></option>
+          </template>
+        </optgroup>
+      </template>
     </select>
- 
-
+  </div>
 </div>
-</div>
-
-
