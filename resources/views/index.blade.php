8380963158c748bffca9e25b229d3005b97f2a59
downs-Jacob <tua04072@gmail.com>
2023-05-21 22:07:43 -0400

    make archive for 40k more mobile friendly

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,33 @@
 @extends ('layouts.layout')
 
 @section('content')
-<div class="pt-4 mx-24 large:mr-20 large:ml-20">
-    <div class="ml-10 mr-10 px-2 py-2 bg-[#D5C3ED] shadow-xl border border-indigo-200 rounded-xl">
-      <div class="grid grid-cols-4 bg-[#EBE2F6] ">
-        <div class="text-center underline">ID</div>
-        <div class="text-center underline">Date</div>
-        <div class="col-span-2 text-center underline">Armies</div>
-      </div>
-     
-
-                @foreach ($games as $game)
-
-                    <a class="grid grid-cols-4 text-center hover:text-gray-400 transition duration-150 ease-in-out" href="/games/{{$game->id}}">
-                        <div class="bg-[#EBE2F6]">
-                            <div class="">
-                                {{ $game->id}}
-                            </div>
-                        </div>
-                        <div class="bg-[#EBE2F6]">
-                            <div class="">
-                                {{$game->created_at->format('M, d, Y')}}
-                            </div>
-                        </div>
-                        <div class="col-span-2 bg-[#EBE2F6]"> 
-                            <div class="">
-                                {{$game->player1_faction}} vs {{$game->player2_faction}}
-                            </div>
-                        </div>
-                    </a>
-
-
-                @endforeach
-
+<div class="pt-4 mx-2 sm:mx-24">
+    <div class="ml-2 mr-2 sm:mr-10 sm:ml-10 px-2 py-2 bg-[#D5C3ED] shadow-xl border border-indigo-200 rounded-xl">
+        <div class="grid grid-cols-4 sm:grid-cols-4 bg-[#EBE2F6]">
+            <div class="text-center underline">ID</div>
+            <div class="text-center underline">Date</div>
+            <div class="col-span-2 text-center underline">Armies</div>
         </div>
+
+        @foreach ($games as $game)
+        <a class="grid grid-cols-4 sm:grid-cols-4 text-center hover:text-gray-400 transition duration-150 ease-in-out" href="/games/{{$game->id}}">
+            <div class="bg-[#EBE2F6]">
+                <div class="">
+                    {{ $game->id}}
+                </div>
+            </div>
+            <div class="bg-[#EBE2F6]">
+                <div class="">
+                    {{$game->created_at->format('M, d, Y')}}
+                </div>
+            </div>
+            <div class="col-span-2 bg-[#EBE2F6]"> 
+                <div class="">
+                    {{$game->player1_faction}} vs {{$game->player2_faction}}
+                </div>
+            </div>
+        </a>
+        @endforeach
     </div>
 </div>
 @endsection
